ebb71dce66926742e7bef133b79453c1b3493770
maccwinter <matthew.winter1@louisiana.edu>
2022-08-24 21:03:39 -0500

    here we go! It worked.

---
+++ beginning.R
@@ -0,0 +1,4 @@
+#I believe I have finally figured out how to set up version control
+#with GitHub and successfully commit and push my changes. It has taken me
+#several hours, but it feels good to have figured it out. I am not done for 
+#the day with my work. Tomorrow I will begin again. 
