90e0992c58f46c972920c33dda8365fb1e8c91af
maccwinter <matthew.winter1@louisiana.edu>
2022-08-24 21:07:12 -0500

    lets see

--- beginning.R
+++ beginning.R
@@ -2,3 +2,5 @@
 #with GitHub and successfully commit and push my changes. It has taken me
 #several hours, but it feels good to have figured it out. I am not done for 
 #the day with my work. Tomorrow I will begin again. 
+#I am now looking into a warning that said "adding ip address to list of known
+#hosts" to see if it always gives that warning. Might have to figure that out. 
